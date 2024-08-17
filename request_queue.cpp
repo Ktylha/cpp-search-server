6fc22cbd890fb31c7b25c2a10d130e835082609b
Павел Кравцов <Ktylha@vk.com>
2024-08-17 22:26:28 +0300

    «Делим проект на файлы» темы «Распределение кода по файлам»

---
+++ request_queue.cpp
@@ -0,0 +1,35 @@
+#include "request_queue.h"
+ 
+ 
+RequestQueue::RequestQueue(const SearchServer& search_server) : search_server_(search_server), count_request_(0)
+{
+}
+ 
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentStatus status)
+{
+    const auto result = search_server_.FindTopDocuments(raw_query, status);
+    AddRequest(result.size());
+    return result;
+}
+ 
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query)
+{
+    const auto result = search_server_.FindTopDocuments(raw_query);
+    AddRequest(result.size());
+    return result;
+}
+ 
+int RequestQueue::GetNoResultRequests() const
+{
+    return min_in_day_ - count_request_;
+}
+ 
+void RequestQueue::AddRequest(int num_request)
+{
+    if (num_request != 0)
+    {
+        ++current_time_;
+        ++count_request_;
+        requests_.push_back({ num_request, current_time_ });
+    }
+}
---
+++ request_queue.h
@@ -0,0 +1,41 @@
+#pragma once
+ 
+#include <string>
+#include <vector>
+#include <deque>
+#include "search_server.h"
+#include "document.h"
+
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server);
+ 
+    template <typename DocumentPredicate>
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+    std::vector<Document> AddFindRequest(const std::string& raw_query);
+    int GetNoResultRequests() const;
+ 
+private:
+    struct QueryResult {
+        int req;
+        uint64_t timestamp;
+    };
+    std::deque<QueryResult> requests_;
+    const SearchServer& search_server_;
+    int count_request_ = 0;
+    uint64_t current_time_ = 0;
+    const static int min_in_day_ = 1440;
+ 
+ 
+    void AddRequest(int num_request);
+        
+};
+ 
+template<typename DocumentPredicate>
+inline std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate)
+{
+    const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+    AddRequest(result.size());
+    return result;
+}
